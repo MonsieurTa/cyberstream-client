104bc144157d4b357e689d32de273215e0571297
William Ta <wta@student.42.fr>
2021-05-21 10:43:31 +0200

    refactor: update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,36 @@
   <v-container class="fill-height">
     <v-row>
       <v-spacer />
-      <v-col cols="4">
-        <h1 class="white--text">Looking for something to watch ?</h1>
-      </v-col>
-      <v-col cols="4" class="d-flex align-center">
-        <v-select
-          label="Category"
-          single-line
-          light
-          rounded
-          outlined
+      <v-col cols="4" class="d-flex flex-column justify-center">
+        <v-text-field
+          dark
+          v-model="pattern"
+          class="flex-grow-0"
+          prepend-inner-icon="mdi-magnify"
+          append-icon="mdi-arrow-right"
+          label="Looking for something to watch ?"
           dense
-          background-color="white"
-        ></v-select>
+          single-line
+          clearable
+          outlined
+          background-color="grey darken-3"
+          color="primary"
+          hide-details
+        />
+        <v-row>
+          <v-spacer />
+          <v-col v-for="name in categoryNames" :key="name" cols="3">
+            <v-checkbox
+              dark
+              v-model="selectedCategories"
+              :key="name"
+              :label="name"
+              :value="name"
+              hide-details
+            />
+          </v-col>
+          <v-spacer />
+        </v-row>
       </v-col>
       <v-spacer />
     </v-row>
@@ -22,11 +39,33 @@
 </template>
 
 <script>
-export default {};
+import service from "@/services/hyper-jackett";
+
+export default {
+  name: "Home",
+  async mounted() {
+    service.fetchCategories().then((categories) => {
+      this.categories = categories;
+    });
+  },
+  data() {
+    return {
+      pattern: "",
+      selectedCategories: [],
+      categories: {},
+    };
+  },
+  computed: {
+    categoryNames() {
+      return Object.keys(this.categories);
+    },
+  },
+  methods: {},
+};
 </script>
 
 <style scoped>
 h1 {
-  font-size: 5em;
+  font-size: 4.5em;
 }
 </style>
