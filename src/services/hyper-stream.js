f04050d58e39c27ab64aeae3b6ba74e09b1fe456
William Ta <wta@student.42.fr>
2021-06-28 12:43:10 +0200

    feat: implement StreamResponse

--- src/services/hyper-stream.js
+++ src/services/hyper-stream.js
@@ -18,7 +18,44 @@ class HyperStream {
           src: { src: media_url, type },
           tracks: { src: subtitles_url }
         });
-      });
+export class StreamResponse {
+  _name = ""
+  _mediaUrl = ""
+  _subtitlesUrls = []
+  _ext = ""
+  _info_hash = ""
+  _error = ""
+
+  constructor(data) {
+    const {
+      name,
+      media_url,
+      subtitles_urls,
+      ext,
+      info_hash,
+      error,
+    } = data
+    this._name = name
+    this._mediaUrl = media_url
+    this._subtitlesUrls = subtitles_urls
+    this._ext = ext
+    this._info_hash = info_hash
+    this._error = error
+  }
+
+  get error() {
+    return this._error;
+  }
+
+  hasSubtitles() {
+    return Array.isArray(this._subtitlesUrls) && this._subtitlesUrls.length > 0;
+  }
+
+  toVideoJSContent() {
+    return {
+      src: { src: this._mediaUrl, type: this._type },
+      tracks: { src: this._subtitlesUrls },
+    }
   }
 }
 
