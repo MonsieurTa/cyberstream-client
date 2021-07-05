4a65e73b26e5063bc757f9faa97e7ca30782d26e
William Ta <wta@student.42.fr>
2021-07-05 14:45:55 +0200

    feat: add forty two icon

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,18 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import IconFortyTwo from '@/components/icons/IconFortyTwo.vue';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    values: {
+      fortytwo: {
+        component: IconFortyTwo,
+        props: {
+          name: 'fortytwo',
+        },
+      },
+    },
+  },
 });
