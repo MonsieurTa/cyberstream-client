2ecd470d6c8f9f46122fdbe88d72b4955bec1e80
William Ta <wta@student.42.fr>
2021-07-05 14:46:19 +0200

    feat: add naviguation guard and basic routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import routes from './routes'
+import Vue from "vue"
+import VueRouter from "vue-router"
+import routes from "./routes"
+import store from "@/store";
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-    routes,
-    // Use the HTML5 history API (i.e. normal-looking routes)
-    // instead of routes with hashes (e.g. example.com/#/about).
-    // This may require some server configuration in production:
-    // https://router.vuejs.org/en/essentials/history-mode.html#example-server-configurations
-    mode: 'history',
-    // Simulate native-like scroll behavior when navigating to a new
-    // route and using back/forward buttons.
-    scrollBehavior(to, from, savedPosition) {
-        if (savedPosition) {
-            return savedPosition
-        } else {
-            return { x: 0, y: 0 }
-        }
-    },
-})
+  routes,
+  // Use the HTML5 history API (i.e. normal-looking routes)
+  // instead of routes with hashes (e.g. example.com/#/about).
+  // This may require some server configuration in production:
+  // https://router.vuejs.org/en/essentials/history-mode.html#example-server-configurations
+  mode: "history",
+  // Simulate native-like scroll behavior when navigating to a new
+  // route and using back/forward buttons.
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },
+});
 
-export default router
+router.beforeEach(async (to, _, next) => {
+  const isAuthenticated = store.state.auth.isAuthenticated;
+  const isAuthPages = to.name === "LogIn" || to.name === "AuthFortyTwo";
+
+  if (!isAuthenticated && !isAuthPages) {
+    next({ name: "LogIn" });
+  } else {
+    next();
+  }
+});
+
+export default router;
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,53 @@
-import Home from "../views/Home.vue"
+import Home from '@/views/Home.vue';
+import LogIn from '@/views/LogIn.vue';
+import AuthFortyTwo from '@/views/AuthFortyTwo.vue';
+import store from '@/store';
+import authentifier from '@/services/authentifier';
 
 export default [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home,
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/logIn',
+    name: 'LogIn',
+    component: LogIn,
+    beforeEnter: async (_, __, next) => {
+      const { isAuthenticated } = store.state.auth;
+
+      if (isAuthenticated) {
+        next({ name: "Home" });
+        return;
+      }
+
+      await authentifier.newAccessToken()
+        .then(({ headers: { authorization } }) => {
+          store.dispatch("auth/authenticate", authorization);
+          renewAccessTokenIn((60 * 15 - 10) * 1000);
+
+          next({ name: "Home" })
+        })
+        .catch(() => next());
     },
-]
+  },
+  {
+    path: '/oauth/fortytwo/callback',
+    name: 'AuthFortyTwo',
+    component: AuthFortyTwo,
+  }
+];
+
+const renewAccessTokenIn = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(async () => {
+      await authentifier.newAccessToken()
+        .then(({ headers: { authorization } }) => {
+          store.dispatch("auth/authenticate", authorization);
+        });
+      resolve();
+      renewAccessTokenIn(ms);
+    }, ms);
+  });
+}
