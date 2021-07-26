2419f7fadefd665489526b8c1482d6e46783939b
William Ta <wta@student.42.fr>
2021-07-26 14:10:02 +0200

    feat: add ErrorSnackBack component

---
+++ src/components/ErrorSnackBack.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-snackbar v-model="openListener" :timeout="4000">
+    {{ msg }}
+    <template v-slot:action="{ attrs }">
+      <v-btn color="blue" text v-bind="attrs" @click="openListener = false">
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: "ErrorSnackBack",
+  props: {
+    msg: String,
+    open: Boolean,
+  },
+  computed: {
+    openListener: {
+      get() {
+        return this.open;
+      },
+      set(v) {
+        this.$emit("update:open", v);
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
